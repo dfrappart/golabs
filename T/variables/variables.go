7cf95741224e35bec9a30f9ec022083675abad0d
dfrappart <david@frappart.com>
2024-08-20 16:25:59 +0200

    add T folder

---
+++ T/variables/variables.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var a = "initial"
+    fmt.Println("la valeur de a est :",a)
+
+    var b, c int = 1, 2
+    fmt.Println("la valeur de b est :",b,", la valeur de c est :", c)
+
+    var d = true
+    fmt.Println("la valeur de d est :",d)
+
+    var e int
+    fmt.Println("la valeur de e est :",e)
+
+    //f := "apple"
+	var f = "apple"
+    fmt.Println("f value is: ",f)
+
+    var pi = 3.141592654
+    fmt.Println("la valeur de pi est :",pi)
+}

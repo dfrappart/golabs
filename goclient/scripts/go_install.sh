111d08be4a5343004e7b064ce9abbfc9d29e1f79
dfrappart <david@frappart.com>
2024-08-20 16:26:27 +0200

    update vagrant machine

--- goclient/scripts/go_install.sh
+++ goclient/scripts/go_install.sh
@@ -1,5 +1,4 @@
-wget https://go.dev/dl/go1.23.0.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.0.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf /tmp/go1.23.0.linux-amd64.tar.gz
 #export PATH=$PATH:/usr/local/go/bin
 #echo $PATH >> /etc/environment
 #echo $PATH >> /etc/profile
